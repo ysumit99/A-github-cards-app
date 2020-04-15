7e7ea876a0355f3ee1c7d22f268b3e4e753afa2d
Sumit Yadav <ysumit99@gmail.com>
2020-04-15 20:33:22 +0530

    A basic github-cards app using axios and React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
-
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
 
 const CardList = (props) => (
   <div>
-    {props.profiles.map(profile => <Card {...profile} />)}
+    {props.profiles.map(profile => <Card key={profile.id} {...profile}  />)}
   </div>
 );
 class Card extends React.Component {
@@ -30,11 +25,11 @@ class Card extends React.Component {
 
 class Form extends React.Component {
   state = { userName: ''};
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(
-      this.userNameInput.current.value
-    )
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(resp.data);
+    this.setState({userName: ''});
   };
 
   render() {
@@ -58,15 +53,21 @@ class App extends React.Component {
   constructor(props){ 
       super(props);
       this.state = {
-        profiles: testData 
+        profiles: []
       };
   }
   
+  addNewProfile = (profileData) => {
+   this.setState(prevState => ({
+     profiles: [...prevState.profiles, profileData]
+   }))
+  };
+
   render() {
   return (
     <div>
       <div className="header">{this.props.title}</div>
-      <Form />
+      <Form  onSubmit={this.addNewProfile} />
       <CardList  profiles = {this.state.profiles}/>
     </div>
   );
